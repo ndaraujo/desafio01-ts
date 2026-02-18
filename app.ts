e6297768ab5380e99576493d47c7224e9b747ee6
Fernando Lima <ndaraujolima@gmail.com>
2026-02-18 12:12:38 -0300

    Método de empréstimo implementado

--- app.ts
+++ app.ts
@@ -5,3 +5,6 @@ const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
 console.log(peopleAccount)
 const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
 console.log(companyAccount)
+companyAccount.getLoan(10000);
+console.log(companyAccount)
+console.log(companyAccount.getBalance())
--- class/CompanyAccount.ts
+++ class/CompanyAccount.ts
@@ -2,11 +2,24 @@ import { DioAccount } from "./DioAccount"
 
 export class CompanyAccount extends DioAccount {
 
+  private statusCompany: boolean = true
+
   constructor(name: string, accountNumber: number){
     super(name, accountNumber)
   }
 
-  getLoan = (): void => {
-    console.log('Voce pegou um empréstimo')
+  getLoan = (valueLoan:number): void => {
+    if(this.validateStatusLoan()){
+      this.balance = this.balance + valueLoan
+      console.log('Empréstimo efetuado com sucesso!')
+    }
+  }
+
+  private validateStatusLoan = (): boolean => {
+    if (this.statusCompany) {
+      return this.statusCompany
+    }
+
+    throw new Error('Conta inválida')
   }
 }
